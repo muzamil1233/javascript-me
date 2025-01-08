2230eb7371012ed94ec1eea51f372b2a6aa41b76
muzamil1233 <muzamiln213@gmail.com>
2025-01-08 16:22:17 +0530

    adding some js

---
+++ .vscode/git&github/Git.js
@@ -0,0 +1,98 @@
+// // Git 
+// // is a version control system is a tool that help us to track the changes in code
+
+
+// // popular
+// // free &open Source
+// // fast & scalable 
+
+
+
+// // mainly it is used to track the history & to callaborate
+
+
+
+// // gitHUb
+
+// // is a website that allows developers to store and manage their code using git
+
+
+// // git config --global user.name ""
+// // git config --global user.email ""
+// // git config --list
+
+
+// // git clone : cloninig a repository on our local local machine (local : laptop or pc)
+
+// // git status : display the status of the code 
+
+// // cd : change directory 
+
+
+
+// // ls -a : all files including hidden files 
+
+
+// // untracked new files that git doesn't track 
+// //  modifeid : changed
+
+// //  staged : file is ready to be commit 
+// //  unmodified : unchanged
+
+
+// // git add . : adds a new file in your working directory to the git staging area:
+// //                   git add < filename > 
+
+
+// //      git add * : adds all new files in your working directory to the git staging area
+
+// //      git commit -m "message" : commits all changes in the staging area to the repository:
+// //                   git commit -am "message"
+
+
+// //                   it is record of change 
+
+
+// //                    git log : shows the history of the commits
+
+
+// //                    push : upload local repo content to remote repository
+// //                    git push  origin main 
+
+// // 
+
+
+//               WEB SOCKETS
+
+// client    >>>>>>>>>>    Server 
+//   it means  once a connection is build b/w client and server it can transfer information at any time then 
+
+//   it is bidirectional &  full duplex
+
+
+//   http is a unidirectional and stateless protocol 
+//   websocket is a bidirectional &  full duplex protocol
+
+
+//   short polling means we are requesting a request and server response in afrequent time 
+
+//   long poling means we are having aling time for a request and server response in
+
+
+
+
+//   server side events  it is unidirection means the information is only sent from server to client not from client to server meams we have have to transfer a lot of data  eg chatgpt
+
+//   but in web sockets the first request client send to server is http request  and replies with status code 101(switchng protocol response) 
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,7 @@
+
+
+function isPalindrome(str){
+    const cleanStr = str.toLower().replace();
+    return cleanStr = cleanStr.split("").reverse().join("")
+}
+console.log(isPalindrome(hello));
