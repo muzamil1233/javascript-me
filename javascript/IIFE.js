38b94ef19e0b4412a83126eeb727c6660534f962
Muzamil Nabi <Muzamin213@gmail.com>
2025-10-09 23:03:01 +0530

    added the iffe function

---
+++ javascript/IIFE.js
@@ -0,0 +1,8 @@
+
+// 🧠 What is an IIFE (Immediately Invoked Function Expression)?
+
+// An IIFE is a function in JavaScript that runs immediately after it is defined.
+(function(msg){
+    const name = "muzamil"
+    console.log(msg,name)
+})("Asalmualaikum")
